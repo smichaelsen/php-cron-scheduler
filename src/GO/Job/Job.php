06c1cad0cb94b6b1cc2be264069b221984f45b44
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-02-10 11:01:11 +0100

    Support late execution by writing a last_execution file

--- src/GO/Job/Job.php
+++ src/GO/Job/Job.php
@@ -11,322 +11,335 @@ use Swift_Message;
 
 abstract class Job
 {
-  /**
-   * The command
-   *
-   * @var string
-   */
-  protected $command;
+    /**
+     * The command
+     *
+     * @var string
+     */
+    protected $command;
 
-  /**
-   * The arguments to be passed to the command
-   *
-   * @var array
-   */
-  protected $args;
+    /**
+     * The arguments to be passed to the command
+     *
+     * @var array
+     */
+    protected $args;
 
-  /**
-   * The compiled command
-   *
-   * @var string
-   */
-  protected $compiled;
+    /**
+     * The compiled command
+     *
+     * @var string
+     */
+    protected $compiled;
 
-  /**
-   * The job start time
-   *
-   * @var int
-   */
-  protected $time;
+    /**
+     * The job start time
+     *
+     * @var int
+     */
+    protected $time;
 
-  /**
-   * The files where the output has to be sent
-   *
-   * @var array
-   */
-  protected $outputs = [];
+    /**
+     * The files where the output has to be sent
+     *
+     * @var array
+     */
+    protected $outputs = [];
 
-  /**
-   * The emails where the output has to be sent
-   *
-   * @var array
-   */
-  private $emails = [];
+    /**
+     * @var string
+     */
+    protected $lastExecutionFile;
 
-  /**
-   * The email address used to send the email
-   *
-   * @var array
-   */
-  private $emailFrom = ['cronjob@server.my' => 'My Email Server'];
+    /**
+     * The emails where the output has to be sent
+     *
+     * @var array
+     */
+    private $emails = [];
 
-  /**
-   * Instance of
-   *
-   * @var Cron\CronExpression
-   */
-  public $execution;
+    /**
+     * The email address used to send the email
+     *
+     * @var array
+     */
+    private $emailFrom = ['cronjob@server.my' => 'My Email Server'];
 
-  /**
-   * Bool that defines if the command has to run in backgroud
-   *
-   * @var bool
-   */
-  public $runInBackground = true;
+    /**
+     * Instance of
+     *
+     * @var \Cron\CronExpression
+     */
+    public $execution;
 
-  /**
-   * Bool that defines if the command passed the truth test
-   *
-   * @var bool
-   */
-  public $truthTest = true;
+    /**
+     * Bool that defines if the command has to run in backgroud
+     *
+     * @var bool
+     */
+    public $runInBackground = true;
+
+    /**
+     * Bool that defines if the command passed the truth test
+     *
+     * @var bool
+     */
+    public $truthTest = true;
 
 
-  /**
-   * Create a new instance of Job
-   *
-   * @param mixed $job
-   * @param array $args
-   * @return void
-   */
-  public function __construct($job, array $args = [])
-  {
-    $this->time = time();
+    /**
+     * Create a new instance of Job
+     *
+     * @param mixed $job
+     * @param array $args
+     * @return void
+     */
+    public function __construct($job, array $args = [])
+    {
+        $this->time = time();
 
-    $this->command = $job;
+        $this->command = $job;
 
-    $this->args = $args;
+        $this->args = $args;
 
-    if (method_exists($this, 'init')) {
-      $this->init();
+        if (method_exists($this, 'init')) {
+            $this->init();
+        }
+
+        $this->compiled = $this->build();
     }
 
-    $this->compiled = $this->build();
-  }
-
-  /**
-   * Get command
-   *
-   * @return array
-   */
-  public function getCommand()
-  {
-    return $this->compiled;
-  }
-
-  /**
-   * Get args
-   *
-   * @return array
-   */
-  public function getArgs()
-  {
-    return $this->args;
-  }
-
-  /**
-   * Define when to run the job
-   *
-   * @param string expression
-   * @return $this
-   */
-  public function at($expression)
-  {
-    $this->execution = CronExpression::factory($expression);
-
-    return $this;
-  }
-
-  /**
-   * Define the execution interval of the job
-   *
-   * @param int $interval - fallback to string '*'
-   * @return GO\Services\Interval
-   */
-  public function every($interval = '*')
-  {
-    return new Interval($this, $interval);
-  }
-
-  /**
-   * Define the file/s where to send the output of the job
-   *
-   * @param string/array $ouput
-   * @param bool $mode
-   * @return $this
-   */
-  public function output($output, $mode = false)
-  {
-    $this->outputs = is_array($output) ? $output : [$output];
-
-    $this->mode = $mode === true ? 'a' : 'w';
-
-    return $this;
-  }
-
-  /**
-   * Get files output
-   *
-   * @return array
-   */
-  public function getFilesOutput()
-  {
-    return $this->outputs;
-  }
-
-  /**
-   * Define the email address/es where to send the output of the job
-   *
-   * @param string/array $email
-   * @return $this
-   */
-  public function email($email)
-  {
-    $this->emails = is_array($email) ? $email : [$email];
-
-    $this->runInBackground = false;
-
-    return $this;
-  }
-
-  /**
-   * Get emails output
-   *
-   * @return array
-   */
-  public function getEmailsOutput()
-  {
-    return $this->emails;
-  }
-
-  /**
-   * Check if the job is due to run
-   *
-   * @return bool
-   */
-  public function isDue()
-  {
-    return $this->execution->isDue() && $this->truthTest === true;
-  }
-
-  /**
-   * Abstract function build and compile the command
-   *
-   */
-  abstract public function build();
-
-  /**
-   * Compile command - finalize with output redirections
-   *
-   * @param string $command
-   * @return string
-   */
-  protected function compile($command)
-  {
-    if (count($this->args) > 0) {
-      foreach ($this->args as $key => $value) {
-        $command .= " {$key} \"{$value}\"";
-      }
+    /**
+     * Get command
+     *
+     * @return array
+     */
+    public function getCommand()
+    {
+        return $this->compiled;
     }
 
-    if (count($this->outputs) > 0) {
-      $command .= ' | tee ';
-      $command .= $this->mode === 'a' ? '-a ' : '';
-      foreach ($this->outputs as $o) {
-        $command .= $o.' ';
-      }
+    /**
+     * Get args
+     *
+     * @return array
+     */
+    public function getArgs()
+    {
+        return $this->args;
     }
 
-    $command .= ' > /dev/null 2>&1';
-    if ($this->runInBackground === true) {
-      $command .= ' &';
+    /**
+     * Define when to run the job
+     *
+     * @param string expression
+     * @return $this
+     */
+    public function at($expression)
+    {
+        $this->execution = CronExpression::factory($expression);
+
+        return $this;
     }
 
-    return $this->compiled = trim($command);
-  }
-
-  /**
-   * Execute the job
-   *
-   * @return string - The output of the executed job
-   */
-  public function exec()
-  {
-    $this->compiled = $this->build();
-    if (is_callable($this->compiled)) {
-      $return = call_user_func($this->command, $this->args);
-      foreach ($this->outputs as $output) {
-        Filesystem::write($return, $output, $this->mode);
-      }
-    } else {
-      $return = exec($this->compiled);
+    /**
+     * Define the execution interval of the job
+     *
+     * @param int $interval - fallback to string '*'
+     * @return \GO\Services\Interval
+     */
+    public function every($interval = '*')
+    {
+        return new Interval($this, $interval);
     }
 
-    if ($this->emails) {
-      $this->sendEmails();
+    /**
+     * Define the file/s where to send the output of the job
+     *
+     * @param string /array $ouput
+     * @param bool $mode
+     * @return $this
+     */
+    public function output($output, $mode = false)
+    {
+        $this->outputs = is_array($output) ? $output : [$output];
+
+        $this->mode = $mode === true ? 'a' : 'w';
+
+        return $this;
     }
 
-    return $return;
-  }
-
-  /**
-   * Send the output to an email address
-   *
-   * @return void
-   */
-  private function sendEmails()
-  {
-    $transport = Swift_MailTransport::newInstance();
-    $mailer = Swift_Mailer::newInstance($transport);
-
-    $message = Swift_Message::newInstance()
-      ->setSubject('Cronjob execution')
-      ->setFrom($this->emailFrom)
-      ->setTo($this->emails)
-      ->setBody('Cronjob output attached')
-      ->addPart('<q>Cronjob output attached</q>', 'text/html');
-
-    foreach ($this->outputs as $file) {
-      $message->attach(Swift_Attachment::fromPath($file));
+    /**
+     * Get files output
+     *
+     * @return array
+     */
+    public function getFilesOutput()
+    {
+        return $this->outputs;
     }
 
-    $mailer->send($message);
-  }
+    /**
+     * Define the email address/es where to send the output of the job
+     *
+     * @param string /array $email
+     * @return $this
+     */
+    public function email($email)
+    {
+        $this->emails = is_array($email) ? $email : [$email];
 
-  /**
-   * Run the command in foreground
-   *
-   * @return void
-   */
-  public function runInForeground()
-  {
-    $this->runInBackground = false;
-  }
+        $this->runInBackground = false;
 
-  /**
-   * Injected config from the scheduler
-   *
-   * @return void
-   */
-  public function setup(array $config)
-  {
-    if (isset($config['emailFrom'])) {
-      $this->emailFrom = $config['emailFrom'];
+        return $this;
     }
-  }
 
-  /**
-   * Delegate execution to truth test if it's due
-   *
-   * @return void
-   */
-  public function when($test)
-  {
-    if (!is_callable($test)) {
-      throw new \Exception('InvalidArgumentException');
+    /**
+     * Get emails output
+     *
+     * @return array
+     */
+    public function getEmailsOutput()
+    {
+        return $this->emails;
     }
-    $this->truthTest = $test();
 
-    return $this;
-  }
+    /**
+     * Check if the job is due to run
+     *
+     * @return bool
+     */
+    public function isDue()
+    {
+        if ($this->lastExecutionFile && is_readable($this->lastExecutionFile)) {
+            $lastExecution = file_get_contents($this->lastExecutionFile);
+            $lastRunDate = $this->execution->getPreviousRunDate();
+            return $lastRunDate->getTimestamp() > $lastExecution && $this->truthTest === true;
+        }
+        return $this->execution->isDue() && $this->truthTest === true;
+    }
+
+    /**
+     * Abstract function build and compile the command
+     *
+     */
+    abstract public function build();
+
+    /**
+     * Compile command - finalize with output redirections
+     *
+     * @param string $command
+     * @return string
+     */
+    protected function compile($command)
+    {
+        if (count($this->args) > 0) {
+            foreach ($this->args as $key => $value) {
+                $command .= " {$key} \"{$value}\"";
+            }
+        }
+
+        if (count($this->outputs) > 0) {
+            $command .= ' | tee ';
+            $command .= $this->mode === 'a' ? '-a ' : '';
+            foreach ($this->outputs as $o) {
+                $command .= $o . ' ';
+            }
+        }
+
+        $command .= ' > /dev/null 2>&1';
+        if ($this->runInBackground === true) {
+            $command .= ' &';
+        }
+
+        return $this->compiled = trim($command);
+    }
+
+    /**
+     * Execute the job
+     *
+     * @return string - The output of the executed job
+     */
+    public function exec()
+    {
+        $this->compiled = $this->build();
+        if (is_callable($this->compiled)) {
+            $return = call_user_func($this->command, $this->args);
+            foreach ($this->outputs as $output) {
+                Filesystem::write($return, $output, $this->mode);
+            }
+        } else {
+            $return = exec($this->compiled);
+        }
+
+        if ($this->emails) {
+            $this->sendEmails();
+        }
+
+        return $return;
+    }
+
+    /**
+     * Send the output to an email address
+     *
+     * @return void
+     */
+    private function sendEmails()
+    {
+        $transport = Swift_MailTransport::newInstance();
+        $mailer = Swift_Mailer::newInstance($transport);
+
+        $message = Swift_Message::newInstance()
+            ->setSubject('Cronjob execution')
+            ->setFrom($this->emailFrom)
+            ->setTo($this->emails)
+            ->setBody('Cronjob output attached')
+            ->addPart('<q>Cronjob output attached</q>', 'text/html');
+
+        foreach ($this->outputs as $file) {
+            $message->attach(Swift_Attachment::fromPath($file));
+        }
+
+        $mailer->send($message);
+    }
+
+    /**
+     * Run the command in foreground
+     *
+     * @return void
+     */
+    public function runInForeground()
+    {
+        $this->runInBackground = false;
+    }
+
+    /**
+     * Injected config from the scheduler
+     *
+     * @return void
+     */
+    public function setup(array $config)
+    {
+        if (isset($config['emailFrom'])) {
+            $this->emailFrom = $config['emailFrom'];
+        }
+        if (isset($config['lastExecutionFile'])) {
+            $this->lastExecutionFile = $config['lastExecutionFile'];
+        }
+    }
+
+    /**
+     * Delegate execution to truth test if it's due
+     *
+     * @return void
+     */
+    public function when($test)
+    {
+        if (!is_callable($test)) {
+            throw new \Exception('InvalidArgumentException');
+        }
+        $this->truthTest = $test();
+
+        return $this;
+    }
 }
--- src/GO/Scheduler.php
+++ src/GO/Scheduler.php
@@ -5,203 +5,209 @@ use GO\Job\JobFactory;
 class Scheduler
 {
 
-  /**
-   * Timezone
-   *
-   * @var string
-   */
-  private $timezone = 'Europe/Dublin';
+    /**
+     * Timezone
+     *
+     * @var string
+     */
+    private $timezone = 'Europe/Dublin';
 
-  /**
-   * The scheduled jobs
-   *
-   * @var array of GO\Job\Job
-   */
-  private $jobs = [];
+    /**
+     * The scheduled jobs
+     *
+     * @var \GO\Job\Job[]
+     */
+    private $jobs = [];
 
-  /**
-   * The executed jobs
-   *
-   * @var array of GO\Job\Job
-   */
-  private $executed = [];
+    /**
+     * The executed jobs
+     *
+     * @var array of GO\Job\Job
+     */
+    private $executed = [];
 
-  /**
-   * The scheduler start time
-   *
-   * @var int
-   */
-  private $time;
+    /**
+     * The scheduler start time
+     *
+     * @var int
+     */
+    private $time;
 
-  /**
-   * Global config for the jobs
-   *
-   * @var array
-   */
-  private $config;
+    /**
+     * Global config for the jobs
+     *
+     * @var array
+     */
+    private $config;
 
 
-  /**
-   * Create a new Scheduler instance and keep optional jobs config
-   *
-   * @param array $config
-   * @return void
-   */
-  public function __construct(array $config = [])
-  {
-    $this->useConfig($config);
+    /**
+     * Create a new Scheduler instance and keep optional jobs config
+     *
+     * @param array $config
+     * @return void
+     */
+    public function __construct(array $config = [])
+    {
+        $this->useConfig($config);
 
-    $this->time = time();
-  }
-
-  /**
-   * Switch between configurations
-   *
-   * @param array $config
-   * @return $this
-   */
-  public function useConfig(array $config)
-  {
-    $this->config = $config;
-
-    return $this;
-  }
-
-  /**
-   * Get current configuration
-   *
-   * @return array
-   */
-  public function getConfig()
-  {
-    return $this->config;
-  }
-
-  /**
-   * Get jobs
-   *
-   * @return array
-   */
-  public function getJobs()
-  {
-    return $this->jobs;
-  }
-
-  /**
-   * Get executed jobs
-   *
-   * @return array
-   */
-  public function getExecutedJobs()
-  {
-    return $this->executed;
-  }
-
-  /**
-   * Set the timezone
-   *
-   * @param string $timezone
-   * @return void
-   */
-  public function setTimezone($timezone)
-  {
-    $this->config['timezone'] = $timezone;
-  }
-
-  /**
-   * PHP job
-   *
-   * @param string $command
-   * @param array $args
-   * @return instance of GO\Job\Job
-   */
-  public function php($command, array $args = [])
-  {
-    return $this->jobs[] = JobFactory::factory(\GO\Job\Php::class, $command, $args);
-  }
-
-  /**
-   * I'm feeling lucky
-   * -----------------
-   * Guess the job to run by the file extension
-   *
-   * @param string $command
-   * @param array $args
-   * @return instance of GO\Job\Job
-   */
-  private function command($command, array $args = [])
-  {
-    $file = basename($command);
-  }
-
-  /**
-   * Raw job
-   *
-   * @param string $command
-   * @return instance of GO\Job\Job
-   */
-  public function raw($command)
-  {
-    return $this->jobs[] = JobFactory::factory(\GO\Job\Raw::class, $command);
-  }
-
-  /**
-   * Ping url
-   *
-   * @param string $url
-   * @return instance of GO\Job\Job
-   */
-  public function ping($command)
-  {
-    return $this->jobs[] = JobFactory::factory(\GO\Job\Ping::class, $command);
-  }
-
-  /**
-   * Closure job
-   *
-   * @param callable $closure
-   * @return instance of GO\Job\Job
-   */
-  public function call($closure)
-  {
-    return $this->jobs[] = JobFactory::factory(\GO\Job\Closure::class, $closure);
-  }
-
-  /**
-   * Move the jobs that can run in background on top of the array
-   * This is done to avoid blocking jobs that can slow down the
-   * execution of other jobs
-   *
-   * @return void
-   */
-  public function jobsInBackgroundFirst()
-  {
-    usort($this->jobs, function ($a, $b) {
-      return $b->runInBackground - $a->runInBackground;
-    });
-  }
-
-  /**
-   * Run the scheduled jobs
-   *
-   * @return array - The output of the executed jobs
-   */
-  public function run()
-  {
-    $output = [];
-
-    // First reorder the cronjobs
-    $this->jobsInBackgroundFirst();
-
-    foreach ($this->jobs as $job) {
-      $job->setup($this->config);
-      if ($job->isDue()) {
-        $output[] = $job->exec();
-        $this->executed[] = $job;
-      }
+        $this->time = time();
     }
 
-    return $output;
-  }
+    /**
+     * Switch between configurations
+     *
+     * @param array $config
+     * @return $this
+     */
+    public function useConfig(array $config)
+    {
+        $this->config = $config;
+
+        return $this;
+    }
+
+    /**
+     * Get current configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * Get jobs
+     *
+     * @return array
+     */
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * Get executed jobs
+     *
+     * @return array
+     */
+    public function getExecutedJobs()
+    {
+        return $this->executed;
+    }
+
+    /**
+     * Set the timezone
+     *
+     * @param string $timezone
+     * @return void
+     */
+    public function setTimezone($timezone)
+    {
+        $this->config['timezone'] = $timezone;
+    }
+
+    /**
+     * PHP job
+     *
+     * @param string $command
+     * @param array $args
+     * @return \GO\Job\Job
+     */
+    public function php($command, array $args = [])
+    {
+        return $this->jobs[] = JobFactory::factory(\GO\Job\Php::class, $command, $args);
+    }
+
+    /**
+     * I'm feeling lucky
+     * -----------------
+     * Guess the job to run by the file extension
+     *
+     * @param string $command
+     * @param array $args
+     * @return instance of GO\Job\Job
+     */
+    private function command($command, array $args = [])
+    {
+        $file = basename($command);
+    }
+
+    /**
+     * Raw job
+     *
+     * @param string $command
+     * @return instance of GO\Job\Job
+     */
+    public function raw($command)
+    {
+        return $this->jobs[] = JobFactory::factory(\GO\Job\Raw::class, $command);
+    }
+
+    /**
+     * Ping url
+     *
+     * @param string $url
+     * @return instance of GO\Job\Job
+     */
+    public function ping($command)
+    {
+        return $this->jobs[] = JobFactory::factory(\GO\Job\Ping::class, $command);
+    }
+
+    /**
+     * Closure job
+     *
+     * @param callable $closure
+     * @return \GO\Job\Job
+     */
+    public function call($closure)
+    {
+        return $this->jobs[] = JobFactory::factory(\GO\Job\Closure::class, $closure);
+    }
+
+    /**
+     * Move the jobs that can run in background on top of the array
+     * This is done to avoid blocking jobs that can slow down the
+     * execution of other jobs
+     *
+     * @return void
+     */
+    public function jobsInBackgroundFirst()
+    {
+        usort($this->jobs, function ($a, $b) {
+            return $b->runInBackground - $a->runInBackground;
+        });
+    }
+
+    /**
+     * Run the scheduled jobs
+     *
+     * @return array - The output of the executed jobs
+     */
+    public function run()
+    {
+        $output = [];
+
+        // First reorder the cronjobs
+        $this->jobsInBackgroundFirst();
+
+        foreach ($this->jobs as $job) {
+            $job->setup($this->config);
+            if ($job->isDue()) {
+                $output[] = $job->exec();
+                $this->executed[] = $job;
+            }
+        }
+
+        if (isset($this->config['lastExecutionFile'])) {
+            $fh = fopen($this->config['lastExecutionFile'], 'w');
+            fwrite($fh, time());
+            fclose($fh);
+        }
+
+        return $output;
+    }
 
 }
